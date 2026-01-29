59fb021975d09b94bff1d679a48de1e7f2910ce0
Gael Castro <gaelcastrolara2006@gmail.com>
2026-01-29 14:42:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const port = 3000;
 
 
 const servidor = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola Mundo');
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hola Pagina Principal');
+    }
+    else if (req.url === '/acerca') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Acerca de');
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Pagina no encontrada');
+    }
 });
 
 
